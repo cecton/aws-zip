f1f2b13b6f57f1fc45428f322bb5098a0b9a6f7c
Cecile Tonglet <cecile.tonglet@cecton.com>
2019-03-23 08:36:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+[![Build Status](https://travis-ci.org/cecton/aws-zip.svg?branch=master)](https://travis-ci.org/cecton/aws-zip)
+[![Latest Version](https://img.shields.io/crates/v/aws-zip.svg)](https://crates.io/crates/aws-zip)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](http://opensource.org/licenses/MIT)
+[![Docs.rs](https://docs.rs/aws-zip/badge.svg)](https://docs.rs/aws-zip)
+[![LOC](https://tokei.rs/b1/github/cecton/aws-zip)](https://github.com/cecton/aws-zip)
+[![Dependency Status](https://deps.rs/repo/github/cecton/aws-zip/status.svg)](https://deps.rs/repo/github/cecton/aws-zip)
+
+aws-zip
+=======
+
+Exactly similar to the `zip -r` command but with a few quirks:
+
+1.  **It doesn't store the modification time:**
+
+    Zipping the same file with the same content but different modification time
+    will produce always the same result. This is useful if you plan to deploy
+    ZIPs on AWS Lambda for example.
+
+2.  **It allows you to compress in bzip2 instead of deflate:**
+
+    This is also useful if you want to reduce your ZIP to upload on AWS.
+
+3.  **Statically linked:**
+
+    Just curl the file in your CI's build and it will work instantly.
+
+4.  **It preserve executable permissions:**
+
+     *  Directories: 755
+     *  Executable files: 755
+     *  Not executable files: 644
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+extern crate walkdir;
+extern crate zip;
+#[macro_use]
+extern crate clap;
+extern crate structopt;
+
+use std::fs::File;
+use std::io::Write;
+use std::iter::Iterator;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use walkdir::WalkDir;
+use zip::write::FileOptions;
+use zip::{DateTime, ZipWriter};
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Compression {
+        Store,
+        Deflate,
+        BZip2
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+pub struct Opt {
+    /// force all file permission
+    #[structopt(short = "m", long = "mode")]
+    pub mode: Option<u32>,
+
+    /// compression method
+    #[structopt(
+        short = "Z",
+        long = "compression-method",
+        default_value = "deflate",
+        raw(
+            possible_values = "&Compression::variants()",
+            case_insensitive = "true"
+        )
+    )]
+    pub compression: Compression,
+
+    /// Target ZIP file will be overwritten
+    #[structopt(name = "ZIPFILE", parse(from_os_str))]
+    zipfile: PathBuf,
+
+    /// Input files or directories
+    #[structopt(name = "FILES", parse(from_os_str), raw(required = "true"))]
+    files: Vec<PathBuf>,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    std::process::exit(match run(opt) {
+        Ok(0) => 0,
+        Ok(errors) => errors,
+        Err(err) => {
+            eprintln!("{}", err);
+            1
+        }
+    })
+}
+
+fn run(opt: Opt) -> Result<i32, std::io::Error> {
+    use Compression::*;
+
+    let mut errors = 0;
+    let method = match opt.compression {
+        Store => zip::CompressionMethod::Stored,
+        Deflate => zip::CompressionMethod::Deflated,
+        BZip2 => zip::CompressionMethod::Bzip2,
+    };
+    let file = File::create(&opt.zipfile)?;
+    let mut zip = ZipWriter::new(file);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .last_modified_time(DateTime::from_date_and_time(1980, 1, 1, 0, 0, 0).unwrap());
+
+    for path in opt.files {
+        let walkdir = WalkDir::new(path);
+        let it = walkdir.into_iter();
+        for entry in it {
+            match entry {
+                Err(err) => {
+                    eprintln!("{}", err);
+                    errors += 1
+                }
+                Ok(entry) => {
+                    let path = entry.path();
+
+                    if path.is_file() {
+                        let metadata = path.metadata().unwrap();
+                        let mode = metadata.permissions().mode();
+                        let options =
+                            options.unix_permissions(if mode & 0o111 != 0 { 0o755 } else { 0o644 });
+
+                        zip.start_file(path.to_str().unwrap(), options)?;
+                        zip.write_all(std::fs::read(&path)?.as_slice())?;
+
+                        println!("{}", path.display());
+                    }
+                }
+            }
+        }
+    }
+    zip.finish()?;
+
+    Ok(errors)
+}
